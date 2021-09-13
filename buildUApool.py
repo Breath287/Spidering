1705d7e0037340f5cdb5f6c718e2e2517b7ca160
Jie Li <jz870780@dal.ca>
2021-09-13 14:05:40 -0300

    jieli

---
+++ buildUApool.py
@@ -0,0 +1,11 @@
+from fake_useragent import UserAgent
+# instantiate an object
+ua = UserAgent()
+
+# randomly get a ie ua
+print(ua.ie)
+print(ua.ie)
+
+# randomly get a firefox ua
+print(ua.firefox)
+print(ua.firefox)
